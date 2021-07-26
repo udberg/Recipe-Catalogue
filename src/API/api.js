f8178c55e3e72e5cd762f3de709273d31ff048c0
udberg <johnuduak066@gmail.com>
2021-07-26 21:37:15 +0100

    Fetch meals from API urls

--- src/API/api.js
+++ src/API/api.js
@@ -23,3 +23,17 @@ const fetchSearchMeals = async => {
 
     throw Error(404);
 };
+
+const fetchMeal = async (id) => {
+    const url = `${API.BASE_URL}/${API.LOOKUP_ENDPOINT}/?i=${id}`;
+    const response = await fetch(url);
+
+    if (response.status === 200) {
+        const data = await response.json();
+        return data;
+
+    }
+    throw Error(404);
+};
+
+export { fetchMeals, fetchSearchMeals, fetchMeal };

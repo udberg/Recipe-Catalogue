2ab55b21b852dee19b66ba5aa234922386ef73a7
udberg <johnuduak066@gmail.com>
2021-07-26 21:31:28 +0100

    Implement search meals function from api

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,19 @@
 import API from '../config/config';
 
 const fetchMeals = async (category) => {
-    const url = `${API.BASE_URL}/${API.FILTER_ENDPOINT}/?c${category}`;
+    const url = `${API.BASE_URL}/${API.FILTER_ENDPOINT}/?c=${category}`;
+    const response = await fetch(url);
+
+    if (response.status === 200) {
+        const data = await response.json();
+        return data;
+    }
+
+    throw Error(404);
+};
+
+const fetchSearchMeals = async => {
+    const url = `${API.BASE_URL}/${API.SEARCH_ENDPOINT}/?s=${query}`;
     const response = await fetch(url);
 
     if (response.status === 200) {

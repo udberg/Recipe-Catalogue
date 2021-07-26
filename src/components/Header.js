b7edb00edf64487db882371e654b87a3f7b6bb93
udberg <johnuduak066@gmail.com>
2021-07-26 23:26:19 +0100

    Fix jsx errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
-import classNames from "classnames";
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import  Search from '../containers/Search';
+import Search from '../containers/Search';
 import styles from '../styles/Header.module.css';
 
 const cx = classNames;
 
 const Header = () => (
-    <nav className={cx(styles.nav)}>
-        <ul className={cx(style.menus)}>
-            <li className={cx(styles.menu__item)}>
-                <Link className={cx(styles.menu__link)} to="/">Home</Link>
-            </li>
-            <li className={cx(styles.menu__item)}>
-                <Link className={cx(styles.menu__link)} to="/categories">Categories</Link>
-            </li>
-        </ul>
+  <nav className={cx(styles.nav)}>
+    <ul className={cx(styles.menus)}>
+      <li className={cx(styles.menu__item)}>
+        <Link className={cx(styles.menu__link)} to="/">Home</Link>
+      </li>
+      <li className={cx(styles.menu__item)}>
+        <Link className={cx(styles.menu__link)} to="/categories">Categories</Link>
+      </li>
+    </ul>
 
-        <div className={cx(styles.menu__search)}>
-            <Search />
-        </div>
-    </nav>
+    <div className={cx(styles.menu__search)}>
+      <Search />
+    </div>
+  </nav>
 );
 
 export default Header;

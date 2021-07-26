66b37d31b7db01b6d8ea0e4496effb8bb5eca71e
udberg <johnuduak066@gmail.com>
2021-07-26 23:59:03 +0100

    Create meal categories and require property types of string

--- src/components/Category.js
+++ src/components/Category.js
@@ -6,20 +6,20 @@ import card from '../styles/card.css';
 const cx = classNames.bind(card);
 
 const Category = ({ category }) => (
-    <div className={cx('card')}>
-        <Link to={`/meals/${category.strCategory}`} className={cx('card-link')}>
-        <img className={cx('card-img')} src={category.strCategoryThumb} alt={category.strCategory} />
-        <div className={cx('card-body')}>
-            <h3 className={cx('card-title')}>
-                {category.strCategory}
-            </h3>
-        </div>
-        </Link>
-    </div>
+  <div className={cx('card')}>
+    <Link to={`/meals/${category.strCategory}`} className={cx('card-link')}>
+      <img className={cx('card-img')} src={category.strCategoryThumb} alt={category.strCategory} />
+      <div className={cx('card-body')}>
+        <h3 className={cx('card-title')}>
+          {category.strCategory}
+        </h3>
+      </div>
+    </Link>
+  </div>
 );
 
 Category.propTypes = {
-    category: PropTypes.objectOf(PropTypes.string).isRequired,
+  category: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default Category;

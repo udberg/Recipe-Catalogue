22dd060540f772fbaf183a55eb4ca748e15c4f4b
udberg <johnuduak066@gmail.com>
2021-07-28 09:40:30 +0100

    Create useEffect for categoryTypes

--- src/containers/Meals.js
+++ src/containers/Meals.js
@@ -3,40 +3,42 @@ import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import classNames from 'classnames';
-import * as actionsType from '../redux/actions/actionsType';
+import * as actionsType from '../redux/actions/actionTypes';
 import Meal from '../components/Meal';
 import { fetchMealsByCategory } from '../redux/actions/thunk';
 import { changeCategory } from '../redux/actions/index';
-import grid from '../styles/grid.css'
+import grid from '../styles/grid.css';
 
 const cx = classNames.bind(grid);
 
 const Meals = ({
-    meals: { meals, status, error }, fetchMealsByCategory, category, changeCategory
+  meals: { meals, status, error }, fetchMealsByCategory, category, changeCategory,
 }) => {
-    const { categoryType } = useParams();
+  const { categoryType } = useParams();
 
-    useEffect(() => {
-        changeCategory(categoryType);
-        if (status === actionsType.IDLE_MEALS || categoryType) {
-            fetchMealsByCategory(categoryType || 'beef');
-        }
-    }, [category]);
-
-    if (status === actionsType.LOADING_MEAL) {
-        return <div>Loading ...</div>;
+  useEffect(() => {
+    changeCategory(categoryType);
+    if (status === actionsType.IDLE_MEALS || categoryType) {
+      fetchMealsByCategory(categoryType || 'beef');
     }
+  }, [category]);
 
-    if (status === actionsType.ERROR_MEALS) {
-        return (
-            <div>
-                Error:
-                {error}
-            </div>
-        );
-    }
+  if (status === actionsType.LOADING_MEALS) {
+    return <div>Loading ...</div>;
+  }
 
+  if (status === actionsType.ERROR_MEALS) {
     return (
-        
-    )
-}
+      <div>
+        Error:
+        {error}
+      </div>
+    );
+  }
+
+  return (
+    <div className={cx('grid', 'grid-column', 'grid-gap-1/5')}>
+      {meals && meals.map((meal) => <Meal key={meal.idMeal} meal={meal} />)}
+    </div>
+  );
+};
